5dad083388eedca7a4d76e92820fe57760797f1d
Oluwasuyi-Timilehin <146850350+Oluwasuyi-Timilehin@users.noreply.github.com>
2024-02-11 00:06:47 +0100

    made some changes

--- vite-project/src/Homepage.jsx
+++ vite-project/src/Homepage.jsx
@@ -1,24 +1,47 @@
-import {AiOutlineArrowLeft} from 'react-icons/ai'
+import { useState } from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+
 const Homepage = () => {
+  const [showSubcontext, setShowSubcontext] = useState(false);
+
+  const handleButtonClick = () => {
+    // Only toggle the visibility of the main content
+    // if the subcontent is currently hidden
+    if (!showSubcontext) {
+      setShowSubcontext(true);
+    }
+  };
+
+  const handleBackButtonClick = () => {
+    setShowSubcontext(false);
+  };
+
   const design = {
     display: "none",
   };
+
   const Label = {
     color: "black",
     backgroundColor: "white",
     height: "30px",
     width: "30px",
   };
+
   return (
     <>
       <section className="">
-        <nav className="flex justify-between w-full items-center px-3 py-2 border-b border-secondary bg-white shadow lg:hidden">
+        <nav className="flex justify-between top-0 sticky w-full items-center px-3 py-2 border-b border-secondary bg-white shadow lg:hidden">
           <p className="font-bold">David</p>
           <img className="h-7 w-7 rounded-full" src="./src/Images/backg.JPG" />
         </nav>
-        <nav className="lg:flex lg:space-x-72 xl:space-x-80">
-          <nav className="hidden px-4 py-4 lg:block md:border md:border-grey rounded-sm lg:fixed lg:w-72 lg:h-screen xl:w-80">
-            <div className="space-y-4 pb-5">
+        <nav className="lg:flex lg:space-x-96">
+          <nav
+            id="mainContent"
+            className={`px-4 py-4 h-screen md:border md:border-grey rounded-sm lg:w-96 lg:fixed ${
+              showSubcontext ? "hidden lg:block" : ""
+            }`}
+          >
+            <div className="space-y-4 container mx-auto pb-5">
               <p className="text-left text-3xl font-medium">History</p>
               <input
                 type="text"
@@ -26,139 +49,109 @@ const Homepage = () => {
                 className="w-full border-2 border-y-2 border-grey border-b-secondary rounded-lg focus:outline-none px-4 py-1"
               />
             </div>
-            <div className="flex space-x-3 w-full items-center border-b border-grey py-2 rounded-xl">
-              <img
-                className="h-10 w-10 rounded-full"
-                src="./src/Images/backg.JPG"
-              />
-              <button className="flex justify-between items-center w-full">
-                <div className="flex flex-col">
-                  <p className="font-medium text-left">David</p>
-                  <p className="text-sm text-left">Send me your Email</p>
+            <div className="container mx-auto">
+              <button
+                className="flex px-2 space-x-3 w-full items-center border-b border-grey py-2 rounded-xl"
+                onClick={handleButtonClick}
+              >
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src="./src/Images/backg.JPG"
+                />
+                <div className="flex justify-between items-center w-full">
+                  <div className="flex flex-col">
+                    <p className="font-medium text-left">David</p>
+                    <p className="text-sm text-left">Send me your Email</p>
+                  </div>
+                  <p className="text-sm">yesterday</p>
                 </div>
-                <p className="text-sm">yesterday</p>
               </button>
-            </div>
-            <div className="flex space-x-3 w-full items-center border-b border-grey py-2 rounded-xl">
-              <img
-                className="h-10 w-10 rounded-full"
-                src="./src/Images/backg.JPG"
-              />
-              <button className="flex justify-between items-center w-full">
-                <div className="flex flex-col">
-                  <p className="font-medium text-left">David</p>
-                  <p className="text-sm text-left">Send me your Email</p>
+              <button className="flex px-2 space-x-3 w-full items-center border-b border-grey py-2 rounded-xl">
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src="./src/Images/backg.JPG"
+                />
+                <div className="flex justify-between items-center w-full">
+                  <div className="flex flex-col">
+                    <p className="font-medium text-left">David</p>
+                    <p className="text-sm text-left">Send me your Email</p>
+                  </div>
+                  <p className="text-sm">yesterday</p>
                 </div>
-                <p className="text-sm">yesterday</p>
               </button>
             </div>
           </nav>
-          <nav className="w-full px-4 py-6 md:px-0 md:py-0">
-            <div className="bg-white hidden sticky top-0 lg:block">
+          <nav
+            id="subcontent"
+            className={`w-full ${showSubcontext ? "" : "hidden"}`}
+          >
+            <div className="bg-white top-0 sticky">
               <div className="flex items-center space-x-3 px-3 py-4">
-                <AiOutlineArrowLeft className='text-xl' />
-                <img src="./src/Images/backg.JPG" className='rounded-full h-10 w-10' />
+                <AiOutlineArrowLeft
+                  className="text-md lg:text-xl"
+                  onClick={handleBackButtonClick}
+                />
+                <img
+                  src="./src/Images/backg.JPG"
+                  className="rounded-full h-8 w-8 lg:h-10 lg:w-10"
+                />
               </div>
             </div>
-            <div className="border px-2 py-3 h-full md:h-full md:px-10 md:py-8">
-              <nav className="">
-                <div className="pb-20 space-y-7">
-                  <p className="bg-grey text-black px-2 py-1 rounded-lg float-left font-medium md:px-4">
-                    Hello there
-                  </p>
-                  <p className="bg-secondary text-black px-2 py-1 rounded-lg float-right font-medium md:px-4">
-                    yess how are you doing
-                  </p>
+            <div className="py-3 h-screen">
+              <div class="p-2">
+                <div class="flex items-start space-x-2">
+                  <img
+                    src="./src/Images/backg.JPG"
+                    class="rounded-full h-8 w-8"
+                  />
+                  <div class="bg-secondary text-white p-3 rounded-lg">
+                    <p>Hello!</p>
+                    <p>How can I help you?</p>
+                  </div>
                 </div>
-                <div className="space-y-7 pb-20">
-                  <p className="bg-grey text-black px-2 py-1 rounded-lg float-left font-medium md:px-4">
-                    Hello there
-                  </p>
-                  <p className="bg-secondary text-black px-2 py-1 rounded-lg float-right font-medium md:px-4">
-                    yess how are you doing
-                  </p>
+                <div class="flex items-end space-x-2 justify-end mt-4">
+                  <div class="bg-primary text-white p-3 rounded-lg">
+                    <p>Hi!</p>
+                    <p>I need some assistance.</p>
+                  </div>
+                  <img
+                    src="./src/Images/backg.JPG"
+                    class="rounded-full h-8 w-8"
+                  />
                 </div>
-                <div className="space-y-7 pb-20">
-                  <p className="bg-grey text-black px-2 py-1 rounded-lg float-left font-medium md:px-4">
-                    Hello there
-                  </p>
-                  <p className="bg-secondary text-black px-2 py-1 rounded-lg float-right font-medium md:px-4">
-                    yess how are you doing
-                  </p>
+              </div>
+
+              <nav className="fixed bottom-2 bg-white w-full md:w-6/12 md:bg-none md:right-5 space-y-3 px-4">
+                <div className="">
+                  <div className="flex space-x-1 ">
+                    <input
+                      type="text"
+                      className="border focus:outline-none px-3 py-0.5 w-8/12 md:py-1.5"
+                    />
+                    <button className="border px-3">Submit</button>
+                  </div>
                 </div>
-                <div className="space-y-7 pb-20">
-                  <p className="bg-grey text-black px-2 py-1 rounded-lg float-left font-medium md:px-4">
-                    Hello there
-                  </p>
-                  <p className="bg-secondary text-black px-2 py-1 rounded-lg float-right font-medium md:px-4">
-                    yess how are you doing
-                  </p>
-                </div>
-                <div className="space-y-7 pb-20">
-                  <p className="bg-grey text-black px-2 py-1 rounded-lg float-left font-medium md:px-4">
-                    Hello there
-                  </p>
-                  <p className="bg-secondary text-black px-2 py-1 rounded-lg float-right font-medium md:px-4">
-                    yess how are you doing
-                  </p>
-                </div>
-                <div className="space-y-7 pb-20">
-                  <p className="bg-grey text-black px-2 py-1 rounded-lg float-left font-medium md:px-4">
-                    Hello there
-                  </p>
-                  <p className="bg-secondary text-black px-2 py-1 rounded-lg float-right font-medium md:px-4">
-                    yess how are you doing
-                  </p>
-                </div>
-                <div className="space-y-7 pb-20">
-                  <p className="bg-grey text-black px-2 py-1 rounded-lg float-left font-medium md:px-4">
-                    Hello there
-                  </p>
-                  <p className="bg-secondary text-black px-2 py-1 rounded-lg float-right font-medium md:px-4">
-                    yess how are you doing
-                  </p>
-                </div>
-                <div className="space-y-7 pb-20">
-                  <p className="bg-grey text-black px-2 py-1 rounded-lg float-left font-medium md:px-4">
-                    Hello there
-                  </p>
-                  <p className="bg-secondary text-black px-2 py-1 rounded-lg float-right font-medium md:px-4">
-                    yess how are you doing
-                  </p>
+                <div className="flex items-center justify-evenly">
+                  <div className="">
+                    <label
+                      for="file"
+                      className="border border-grey px-2 py-0.5 rounded-sm"
+                      style={Label}
+                    >
+                      Select File
+                      <input type="file" id="file" style={design} />
+                    </label>
+                  </div>
+                  <div className=" space-x-">
+                    <button className="text-sm border py-1 bg-primary text-white rounded-sm font-semibold px-2">
+                      Encrypt
+                    </button>
+                    <button className="text-sm border py-1 bg-primary text-white rounded-sm font-semibold px-2">
+                      Decrypt
+                    </button>
+                  </div>
                 </div>
               </nav>
-            </div>
-          </nav>
-
-          <nav className="fixed bottom-0 bg-white w-full md:w-6/12 md:bg-none md:right-5 space-y-3 px-4">
-            <div className="">
-              <div className="flex space-x-1 ">
-                <input
-                  type="text"
-                  className="border focus:outline-none px-3 py-0.5 w-8/12 md:py-1.5"
-                />
-                <button className="border px-3">Submit</button>
-              </div>
-            </div>
-            <div className="flex items-center justify-evenly">
-              <div className="">
-                <label
-                  for="file"
-                  className="border border-grey px-2 py-0.5 rounded-sm"
-                  style={Label}
-                >
-                  Select File
-                  <input type="file" id="file" style={design} />
-                </label>
-              </div>
-              <div className=" space-x-">
-                <button className="text-sm border py-1 bg-primary text-white rounded-sm font-semibold px-2">
-                  Encrypt
-                </button>
-                <button className="text-sm border py-1 bg-primary text-white rounded-sm font-semibold px-2">
-                  Decrypt
-                </button>
-              </div>
             </div>
           </nav>
         </nav>
@@ -166,4 +159,5 @@ const Homepage = () => {
     </>
   );
 };
+
 export default Homepage;
--- vite-project/src/SignUp.jsx
+++ vite-project/src/SignUp.jsx
@@ -1,33 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom"
-const SignUp=()=>{
-
-    return(
-      <>
-       <section className="mx-3 items-center md:flex md:flex-col md:justify-center md:bg-white ">
-          <nav className="mt-36 flex flex-col justify-center space-y-10 md:mt-40">
-            <p className="text-left text-2xl first-letter:text-4xl font-semibold md:text-center md:text-4xl">Sign up</p>
-            <div className="flex flex-col justify-center space-y-5 md:border md:border-grey md:shadow-xl md:px-8 md:rounded-xl  md:py-7 md:w-96">
-                <div className="flex flex-col space-y-2">
-                  <label className="text-lg text-left md:text-xl">Email</label>
-                  <input type="email" placeholder="mail@mailer.com" className="border border-grey rounded-lg px-5 py-2 focus:outline-none focus:border-secondary" />
-                </div>
-                <div className="flex flex-col space-y-2">
-                  <label className="text-lg text-left md:text-xl">Password</label>
-                  <input type="password" placeholder="Password" className="border border-grey rounded-lg px-5 py-2 focus:outline-none focus:border-secondary" />
-                </div>
-                <button className="font-bold bg-primary text-white py-2 rounded-lg ">Sign In</button>
-                <div className="flex justify-center items-center space-x-3">
-                  <i className="border border-b-1 border-grey w-full"></i>
-                  <p className="text-center">or</p>
-                  <i className="border border-b-1 border-grey w-full"></i>
-                </div>
-                <button className="font-bold border border-grey text-black py-2 rounded-lg "><Link to="/log">Continue with gmail</Link></button>
+import { Link } from "react-router-dom";
+const SignUp = () => {
+  return (
+    <>
+      <section className="w-full h-screen flex flex-col justify-center md:bg-white ">
+        <nav className="px-4 container mx-auto flex flex-col justify-center items-center space-y-10">
+          <p className="text-left text-2xl first-letter:text-4xl font-semibold md:text-center md:text-4xl">
+            Sign up
+          </p>
+          <form className="flex w-full flex-col justify-center space-y-5 md:border md:border-grey md:shadow-xl md:px-8 md:rounded-xl  md:py-7 md:w-96">
+            <div className="flex flex-col space-y-2">
+              <label className="text-lg text-left md:text-xl">Email</label>
+              <input
+                type="email"
+                placeholder="mail@mailer.com"
+                className="border border-grey rounded-lg px-5 py-2 focus:outline-none focus:border-secondary"
+              />
             </div>
-            
-          </nav>
-       </section>
-      </>
-    )
-  }
+            <div className="flex flex-col space-y-2">
+              <label className="text-lg text-left md:text-xl">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                className="border border-grey rounded-lg px-5 py-2 focus:outline-none focus:border-secondary"
+              />
+            </div>
+            <button className="font-bold bg-primary text-white py-2 rounded-lg ">
+              Sign In
+            </button>
+            <div className="flex justify-center items-center space-x-3">
+              <i className="border border-b-1 border-grey w-full"></i>
+              <p className="text-center">or</p>
+              <i className="border border-b-1 border-grey w-full"></i>
+            </div>
+            <button className="font-bold border border-grey text-black py-2 rounded-lg ">
+              <Link to="/log">Continue with gmail</Link>
+            </button>
+          </form>
+        </nav>
+      </section>
+    </>
+  );
+};
 export default SignUp;
